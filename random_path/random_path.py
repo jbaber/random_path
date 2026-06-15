2620afd09f18b29de95d5fb5370a47bda0700153
John Baber-Lucero <cornsilkprairie@frundle.com>
2026-06-14 22:15:50 -0400

    Add --only-images and --images-only

--- random_path/random_path.py
+++ random_path/random_path.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass
 from docopt import docopt
+from typing import Callable
+from typing import Optional
+from typing import Tuple
 import csv
+import filetype
 import hashlib
 import io
 import json
@@ -27,18 +32,42 @@ Options:
   -p, --path-blacklist=<filename>   File full of paths to be skipped
   -o, --output-format=<style>       {2}
                                     [DEFAULT: just-filename]
+  -i, --images-only                 Only yield image files
+      --only-images                 Synonym for images-only
   -v, --version                     Show version
   -e, --extension=<.txt>            Only yield files with this extension
                                     (case insensitive)
-
+  -m, --max-tries=<max>             How many paths to fail satisfying
+                                    conditions like --images-only or
+                                    path-blacklist before giving up.
+                                    [DEFAULT: 1000]
   -n, --info                        Print info level logging
   -d, --debug                       Print debug level logging
 """.format(sys.argv[0], DEFAULT_ROOT, OUTPUT_FORMATS_LIST)
 
 
+class Condition:
+    def __init__(self, condition, description: str):
+        self.condition = condition
+        self.description =description
+
+    def satisfied(self, x) -> bool:
+        return self.condition(x)
+
+    @staticmethod
+    def true():
+        return Condition(condition=lambda x: True, description="true")
+
+
+
 class NoGoodFileFound(FileNotFoundError):
     pass
 
+def conditions_met(conditions, testee) -> Tuple[bool, str]:
+    for condition in conditions:
+      if not condition.satisfied(testee):
+        return (False, condition.description)
+    return (True, "")
 
 def sha384(filename):
   BLOCKSIZE = 65536
@@ -51,62 +80,34 @@ def sha384(filename):
   return hasher.hexdigest()
 
 
-def random_non_repeating_filenames(root, *, blacklist=None,
-    conditions=None):
-  if conditions == None:
-    conditions = []
-  if blacklist != None:
-    conditions.append(lambda x: x not in blacklist)
-
-  # TODO Consider how large this gets and do write
-  # to disk if it gets too big or do some other intelligent
-  # optimizations
-  already_yielded = []
-  new_filename = random_file(root,
-      file_condition = lambda x:
-        x not in already_yielded and
-        all([condition(x) for condition in conditions])
-  )
-  while new_filename != None:
-    already_yielded.append(new_filename)
-    yield new_filename
-    new_filename = random_file(root,
-        file_condition = lambda x:
-          x not in already_yielded and
-          all([condition(x) for condition in conditions])
-    )
-
-
-def conditions_met(*, testee, conditions) -> bool:
-    for condition in conditions:
-        if not condition(testee):
-            return False
-    return True
-
-
 def random_file(
     *,
     root: pathlib.Path,
-    file_conditions=None,
-    dir_conditions=None
+    file_conditions: Optional[list[Condition]],
+    dir_conditions: Optional[list[Condition]]
 ) -> pathlib.Path:
   """
   Raises NoGoodFileFound if no regular file can be found satisfying all
   `file_conditions` whose parent directories all satisfy all `dir_conditions`
   """
   if file_conditions == None:
-    file_conditions = [lambda x: True]
+    file_conditions = [Condition.true()]
   if dir_conditions == None:
-    dir_conditions = [lambda x: True]
-  if not conditions_met(testee=root, conditions=dir_conditions):
-    raise NoGoodFileFound(f"'{root}' doesn't satisfy all dir_conditions")
-  if root.is_file():
-    if conditions_met(testee=root, conditions=file_conditions):
-      return root
-    else:
-      raise NoGoodFileFound(f"'{root}' that doesn't satisfy all file_conditions")
-  if not root.is_dir():
-    return NoGoodFileFound(f"'{root}' is not a directory or regular file")
+    dir_conditions = [Condition.true()]
+
+  if root.is_dir():
+    sat = conditions_met(dir_conditions, testee=root)
+    if not sat[0]:
+      raise NoGoodFileFound(f"'{root}' doesn't satisfy {sat[1]} condition")
+
+  elif root.is_file():
+    sat = conditions_met(file_conditions, testee=root)
+    if not sat[0]:
+      raise NoGoodFileFound(f"'{root}' doesn't satisfy {sat[1]} condition")
+    return root
+
+  else:
+    raise NoGoodFileFound(f"'{root}' is not a directory or regular file")
 
   # Now it's a directory for sure
   logging.info(f"Searching '{root}'")
@@ -114,23 +115,26 @@ def random_file(
 
   # Randomize the list
   all_items = random.sample(all_items, len(all_items))
+
   for dir_or_file in all_items:
     logging.debug(f"Checking '{dir_or_file}'")
     dir_or_file = root / dir_or_file
     if dir_or_file.is_file():
-      logging.info("It's a regular file")
-      if conditions_met(testee=dir_or_file, conditions=file_conditions):
+      logging.debug("It's a regular file")
+      sat = conditions_met(testee=dir_or_file, conditions=file_conditions)
+      if sat[0]:
         logging.info("Conditions met")
         return dir_or_file
     if dir_or_file.is_dir():
-        logging.debug("It's a directory")
-        if conditions_met(testee=dir_or_file, conditions=dir_conditions):
-          logging.info("Conditions met")
-          return random_file(
-            root=dir_or_file,
-            file_conditions=file_conditions,
-            dir_conditions=dir_conditions
-          )
+      logging.debug("It's a directory")
+      sat = conditions_met(testee=dir_or_file, conditions=dir_conditions)
+      if sat[0]:
+        logging.info("Conditions met")
+        return random_file(
+          root=dir_or_file,
+          file_conditions=file_conditions,
+          dir_conditions=dir_conditions
+        )
 
   raise NoGoodFileFound("No file satisfies all file_coditions and has parent directories all satisfy dir_conditions")
 
@@ -145,6 +149,9 @@ def main():
   else:
     logging.basicConfig(level=logging.WARNING)
 
+  if args["--only-images"]:
+    args["--images-only"] = True
+
   try:
     root = pathlib.Path(args["<DIR>"])
   except TypeError:
@@ -154,28 +161,42 @@ def main():
   if _format not in OUTPUT_FORMATS:
     print("Only allowed formats are:\n  {}\nreceived '{}'".format(
         OUTPUT_FORMATS_LIST, _format))
-    exit(1)
+    sys.exit(1)
 
   file_conditions = []
+  dir_conditions = []
   blacklist = []
   if args["--sha384-blacklist"] != None:
     with open(args["--sha384-blacklist"]) as f:
       sha384_blacklist = f.read().split()
-      file_conditions.append(lambda x: sha384(x) not in sha384_blacklist)
+      file_conditions.append(Condition(condition=lambda x: sha384(x) not in sha384_blacklist, description="hash"))
+  if args["--images-only"]:
+    file_conditions.append(Condition(condition=lambda x: filetype.is_image(x), description="image"))
   if args["--path-blacklist"] != None:
     with open(args["--path-blacklist"]) as g:
       blacklist = g.read().split()
   if args["--extension"] != []:
     for extension in args["--extension"]:
-      file_conditions.append(lambda x: x.lower().endswith(extension.lower()))
+      file_conditions.append(Condition(condition=lambda x: x.lower().endswith(extension.lower()), description="extension"))
 
+  tries = 0
+  max_tries = int(args["--max-tries"])
   while True:
     try:
-        print(random_file(root=root, file_conditions=file_conditions))
+        print(
+          random_file(
+            root=root,
+            file_conditions=file_conditions,
+            dir_conditions=dir_conditions
+          )
+        )
         sys.exit(0)
     except NoGoodFileFound:
         logging.info("Failed in this path, try again")
-        time.sleep(.1)
+        tries += 1
+        if tries >= max_tries:
+          print(f"Tried {tries} times, but nothing ever matched all conditions", file=sys.stderr)
+          sys.exit(2)
 
   """
   for _next in random_non_repeating_filenames(root,
